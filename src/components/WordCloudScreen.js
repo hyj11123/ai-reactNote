07f816c977bf84bff19d837f998bdd3bbc9c0fa3
hyj11123 <rpgitemk1234@naver.com>
2020-04-18 15:46:28 +0900

    Craete AppBar

---
+++ src/components/WordCloudScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+class WordCloudScreen extends React.Component {
+    render() {
+        return (
+            <Card>
+                <CardContent>This Page is WordCloudScreen.</CardContent>
+            </Card>
+        );
+    }
+}
+
+/**
+ *  {패키지명 as 사용할이름}
+ *
+ */
+
+export default WordCloudScreen;
